5bdca9a2293a0ff56f17748c71bcd1f36a28cd17
Veniel Dorestave <veniel@noukod.com>
2019-01-29 15:22:43 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,7 +13,7 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
-end
+
 def self.search(q)
     if q.present?
       where('NAME like ?', "%#{q}%")
